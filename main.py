c47eb667c8b06717785810ff37273264ca32a9ba
soominimini <lovesoomindl@naver.com>
2025-02-05 21:21:08 +0100

    brown_bear_modify

--- main.py
+++ main.py
@@ -45,10 +45,8 @@ bed_visited = [False]
 
 baby_chair_visit_check = False
 table_visit = [0, 0, 0]
-chair_visit =[0, 0, 0]
+chair_visit = [0, 0, 0]
 bed_visit = [0, 0, 0]
-
-
 
 show_text = False
 arr_visit = [False, False, False]
@@ -938,7 +936,7 @@ def chairs():
 
 @app.route('/girl/dad_chair')
 def dad_chairs():
-    dad_chair('girl')
+    dad_chair()
     return render_template('dad_chair_page.html', show_text=show_text)
 
 
@@ -1458,7 +1456,6 @@ def story_speak(msg):
     talktext_pub.publish(msg)
 
 
-
 ######################################################################################### break, at talks     ############################################################################################
 #
 
@@ -1505,6 +1502,15 @@ def story_young3():
 @app.route('/brown_bear4')
 def story_young4():
     return render_template('brown_bear4.html')
+@app.route('/brown_bear5')
+def story_young5():
+    return render_template('brown_bear5.html')
+@app.route('/brown_bear6')
+def story_young6():
+    return render_template('brown_bear6.html')
+@app.route('/brown_bear8')
+def story_young8():
+    return render_template('brown_bear8.html')
 
 
 @socketio.on('brown_talk')
@@ -1602,13 +1608,7 @@ def inference_fucn():
     return render_template('inference/inference1.html')
 
 
-
-
-
-
-
 #
-
 
 
 @socketio.on('start_talk')
@@ -1619,30 +1619,35 @@ def first_talk_robot_interactive():
     print("first_talk_robot_interactive")
     talktext_pub.publish("Lets start the story!")
 
+
 @socketio.on('first_page')
 def second():
     # global talktext_pub
     # rospy.sleep(2.0)
     talktext_pub.publish("Once upon a time lived Goldilocks and The Three Bears.")
 
+
 @socketio.on('girl_lodge')
 def third_girl():
     # global talktext_pub
-    talktext_pub.publish("One day, Goldilocks went for a walk in the forest and found a house. She knocked, and when nobody answered, she decided to go inside.")
+    talktext_pub.publish(
+        "One day, Goldilocks went for a walk in the forest and found a house. She knocked, and when nobody answered, she decided to go inside.")
+
 
 @socketio.on('girl_table')
 def table_main(msg):
-    print("msg: ",msg)
+    print("msg: ", msg)
     selected_lodge = msg
-    print("porridge_visited: ",porridge_visited[0])
+    print("porridge_visited: ", porridge_visited[0])
 
     socketio.emit('checking_visit', "String ", broadcast=True)
-    print("msg: ",selected_lodge)
+    print("msg: ", selected_lodge)
 
-    if(porridge_visited[0]==False and selected_lodge != " "):
+    if (porridge_visited[0] == False and selected_lodge != " "):
         # If this is the first visit on this page, robot speaks to choose one bowl
         rospy.sleep(2.0)
         talktext_pub.publish("At the table, there were three bowls of porridge. Goldilocks was hungry.")
+
 
 @socketio.on('dad_porridge')
 def dad_porridge():
@@ -1651,6 +1656,7 @@ def dad_porridge():
     porridge_visited[0] = True
     talktext_pub.publish("Goldilocks tasted the porridge from the large bowl. This porridge is too salty")
 
+
 @socketio.on('mom_porridge')
 def mom_porridge():
     emotionShow_pub.publish("QT/confused")
@@ -1658,38 +1664,34 @@ def mom_porridge():
     porridge_visited[0] = True
     talktext_pub.publish("Goldilocks tasted the porridge from the medium sized bowl. “This porridge is too sweet!")
 
+
 @socketio.on('baby_porridge')
 def baby_porridge():
     emotionShow_pub.publish("QT/happy")
     gesturePlay_pub.publish("QT/happy")
     porridge_visited[0] = True
-    talktext_pub.publish("Goldilocks tasted the porridge from the small bowl. “This porridge is just right,” Goldilocks said and ate it all up.")
+    talktext_pub.publish(
+        "Goldilocks tasted the porridge from the small bowl. “This porridge is just right,” Goldilocks said and ate it all up.")
 
 
 @socketio.on('chair')
 def chair_main():
     print("chair connected")
-    print("chair_visited: ",chair_visited[0])
-    print("chair_visit: ",chair_visit)
+    print("chair_visited: ", chair_visited[0])
+    print("chair_visit: ", chair_visit)
     socketio.emit('number', chair_visit, broadcast=True)
-    if(chair_visited[0]==False):
+    if (chair_visited[0] == False):
         talktext_pub.publish("Goldilocks felt tired, so Goldilocks walked into the living room and saw three chairs.")
 
 
 @socketio.on('dad_chair')
-def dad_chair(msg_selected_character):
+def dad_chair():
     print("here")
     print("here 2")
     print("chair_visit: ", chair_visit)
-    print("msg_selected_character: ",msg_selected_character)
-    if msg_selected_character =='boy':
-        emotionShow_pub.publish("QT/confused")
-        gesturePlay_servc("interact_meh", 2)
-        talktext_pub.publish("Goldilocks sat in the large chair to rest his feet. “This chair is too big!")
-    else:
-        emotionShow_pub.publish("QT/confused")
-        gesturePlay_pub.publish("uwaterloo-1/kickstart/hmm")
-        talktext_pub.publish("Goldilocks sat in the large chair to rest her feet. “This chair is too big!")
+    emotionShow_pub.publish("QT/confused")
+    gesturePlay_pub.publish("uwaterloo-1/kickstart/hmm")
+    talktext_pub.publish("Goldilocks sat in the large chair to rest her feet. “This chair is too big!")
     chair_visited[0] = True
 
 
@@ -1705,8 +1707,8 @@ def mom_chair():
 @socketio.on('baby_chair')
 def baby_chair():
     global baby_chair_visit_check
-    print("chair_visit: ",chair_visit)
-    if(baby_chair_visit_check==False):
+    print("chair_visit: ", chair_visit)
+    if (baby_chair_visit_check == False):
         # To avoid speak again, after coming back from the 'baby_chair2'
         emotionShow_pub.publish("QT/happy")
         gesturePlay_pub.publish("QT/happy")
@@ -1717,23 +1719,24 @@ def baby_chair():
         print("this is revisit from the next page")
         print("Should do nothing")
 
+
 @socketio.on('baby_chair2')
 def baby_chair2():
     talktext_pub.publish("Just as Goldilocks settled down into the chair to rest, it broke into pieces!")
 
 
-
 @socketio.on('bed')
 def bed_main():
-    print("bed_visited: ",bed_visited[0])
+    print("bed_visited: ", bed_visited[0])
     socketio.emit('number', bed_visit, broadcast=True)
-    if(bed_visited[0]==False):
+    if (bed_visited[0] == False):
         # If this is the first visit on this page, robot speaks to choose one bowl
         print("bed enter")
         talktext_pub.publish("By now, Goldilocks was very tired, Goldilocks went upstairs to the bedroom.")
 
+
 @socketio.on('dad_bed')
-def dad_bed_func( ):
+def dad_bed_func():
     emotionShow_pub.publish("QT/confused")
     gesturePlay_pub.publish("uwaterloo-1/kickstart/cross_arm")
     bed_visited[0] = True
@@ -1754,7 +1757,8 @@ def baby_bed_func():
     rospy.sleep(2.5)
     emotionShow_pub.publish("QT/yawn")
     bed_visited[0] = True
-    talktext_pub.publish("Goldilocks lay down on the small bed. This bed is just right. Goldilocks curled up and fell asleep.")
+    talktext_pub.publish(
+        "Goldilocks lay down on the small bed. This bed is just right. Goldilocks curled up and fell asleep.")
 
 
 # # # # # # # # # # # # Bear  # # # # # # # # # # # # # # # # # # #
@@ -1763,18 +1767,21 @@ def bear_1st_func():
     talktext_pub.publish("As Goldilocks was sleeping, The Three Bears came home.")
     porridge_visited[0] = False
     chair_visited[0] = False
-    bed_visited[0] =False
+    bed_visited[0] = False
+
 
 @socketio.on('bear_2nd')
 def bear_2nd_func():
     talktext_pub.publish("Someone’s been eating my porridge, growled Daddy Bear.")
-    rospy.sleep(4.5)
+    rospy.sleep(4)
     # audioPlay_servc("QT/growl_3", "")
     audioPlay_pub.publish('QT/growl_3')
+
 
 @socketio.on('bear_3rd')
 def bear_3rd_func():
     talktext_pub.publish("Someone’s been eating my porridge, said Mummy Bear.")
+
 
 @socketio.on('bear_4th')
 def bear_4th_func():
@@ -1782,15 +1789,18 @@ def bear_4th_func():
     rospy.sleep(4.0)
     audioPlay_pub.publish('QT/cry_10')
 
+
 @socketio.on('bear_5th')
 def bear_5th_func():
     talktext_pub.publish("Someone’s been sitting in my chair!. Growled Daddy Bear.")
     rospy.sleep(4.0)
     audioPlay_pub.publish('QT/growl_3')
 
+
 @socketio.on('bear_6th')
 def bear_6th_func():
     talktext_pub.publish("Someone’s been sitting in my chair!” said Mummy Bear.")
+
 
 @socketio.on('bear_7th')
 def bear_7th_func():
@@ -1798,13 +1808,19 @@ def bear_7th_func():
     rospy.sleep(3.5)
     audioPlay_pub.publish('QT/cry_10')
 
+
 @socketio.on('bear_8th')
 def bear_8th_func():
-    talktext_pub.publish("When they got upstairs to the bedroom, Daddy Bear growled. Someone’s been sleeping on my bed.")
+    talktext_pub.publish(
+        "When they got upstairs to the bedroom, Daddy Bear growled. Someone’s been sleeping on my bed.")
+    rospy.sleep(2.0)
+    audioPlay_pub.publish('QT/growl_3')
+
 
 @socketio.on('bear_9th')
 def bear_9th_func():
     talktext_pub.publish("Someone’s been sleeping on my bed too, said the Mummy Bear")
+
 
 @socketio.on('bear_10th')
 def bear_10th_func():
@@ -1812,17 +1828,18 @@ def bear_10th_func():
     rospy.sleep(3.5)
     audioPlay_pub.publish('QT/cry_10')
 
+
 @socketio.on('bear_11th')
 def bear_11th_func():
     talktext_pub.publish("Just then Goldilocks woke up and saw The Three Bears. Help!. She screamed.")
     rospy.sleep(5)
     audioPlay_pub.publish('QT/scream_low')
 
+
 @socketio.on('bear_12th')
 def bear_12th_func():
-    talktext_pub.publish("Goldilocks ran down the stairs and into the forest. And she never went back into the woods again.")
-
-
+    talktext_pub.publish(
+        "Goldilocks ran down the stairs and into the forest. And she never went back into the woods again.")
 
 
 ######################################################################################### Negin     ############################################################################################
